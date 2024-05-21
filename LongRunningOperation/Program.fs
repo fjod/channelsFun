80492331e5de90669fad9555db8c53f13d75ddc7
Michael <fourjustoneday@yandex.ru>
2024-05-21 15:26:51 +0300

    initial

---
+++ LongRunningOperation/Program.fs
@@ -0,0 +1,11 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System.Threading
+
+printfn "Long operation started"
+
+for i = 0 to 10 do
+  Thread.Sleep(333)
+  printfn $"{i*10} completed"
+  
+printfn "Done"
---
+++ RunnerCs/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, World!");
+
+var ch = Channel.CreateBounded<int>( new BoundedChannelOptions(3)
+{
+    SingleWriter = true,
+    SingleReader = false,
+    FullMode = BoundedChannelFullMode.Wait
+});
+
+List<Task> consumers = new List<Task>();
+for (int i = 0; i < 3; i++)
+{
+    var consumer = Task.Run(async () =>
+    {
+        await foreach (var message in ch.Reader.ReadAllAsync())
+        {
+            Console.WriteLine($"Starting {message}-th task");
+            await StartAndWaitForProcess();
+        }
+    });
+    
+    consumers.Add(consumer);
+}
+
+for (int i = 0; i < 50; i++)
+{
+    while (!ch.Writer.TryWrite(i))
+    {
+        await Task.Delay(100);
+    }
+    
+}
+ch.Writer.Complete();
+await Task.WhenAll(consumers);
+
+async Task StartAndWaitForProcess()
+{
+    var processStartInfo = new ProcessStartInfo("job\\LongRunningOperation.exe");
+    processStartInfo.RedirectStandardError = true;
+    processStartInfo.UseShellExecute = false;
+    var ret = Process.Start(processStartInfo);
+    await ret!.WaitForExitAsync();
+}
